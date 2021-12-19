cf8b2bdd798af2b45ca79e5750f1b5d89d99ee3e
Ricardo Matsui <ricmatsui@gmail.com>
2021-12-18 19:45:15 -0800

    Added temporal web compose, autopi VPN, proxy for services

--- tasksfile.js
+++ tasksfile.js
@@ -1,7 +1,21 @@
 const { sh, cli, rawArgs } = require('tasksfile')
+const path = require('path')
+
+const setup = () => {
+    sh('pipenv run ansible-galaxy collection install -r requirements.yml');
+}
 
 const deploy = () => {
     sh(`pipenv run ansible-playbook playbook.yml ${rawArgs().join(' ')}`, { nopipe: true });
 }
 
-cli({ deploy });
+const temporal = {
+    web: () => {
+        sh(`sops exec-env env.yml 'docker-compose up'`, {
+            cwd: path.join(__dirname, 'temporalite'),
+            nopipe: true,
+        });
+    },
+}
+
+cli({ setup, deploy, temporal });
